db3281c58a6174412af6d3a35f3e4b4e4812c9c3
Rayanne Macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-04-08 20:29:33 -0300

    adicionando o lab1

---
+++ lab1/LendoUmByte.java
@@ -0,0 +1,7 @@
+public class LendoUmByte {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Digite um caractere:");
+        System.out.println(System.in.read());
+    }
+}
+
