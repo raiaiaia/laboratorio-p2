227a7e4b55b7f469e4f89232783f15f82aaeba11
rayanne macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-04-08 20:40:58 -0300

    Delete lab1 directory

--- lab1/LendoUmByte.java
+++
@@ -1,7 +0,0 @@
-public class LendoUmByte {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Digite um caractere:");
-        System.out.println(System.in.read());
-    }
-}
-
