be70aa744e773fdded6ce2a0a6933294f9a32dda
122110702 DANIELLY RAYANNE MACEDO LIMA <danielly.rayanne.macedo.lima@lcc2-27.lcc.ufcg.edu.br>
2023-04-11 09:43:08 -0300

    adicionando lab03

---
+++ lab03/src/agenda/Contato.java
@@ -0,0 +1,15 @@
+package agenda;
+
+public class Contato {
+
+    private String nome;
+    private String sobrenome;
+    private String numero;
+
+    public Contato(String nome, String sobrenome, String numero) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.numero = numero;
+    }
+
+}
